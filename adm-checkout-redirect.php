73c9d28aad22095e550dafe24a3dc50910de86f0
daniel_v <daniel@dj3dw.com>
2022-08-23 13:57:09 +1000

    add proper updater

--- adm-checkout-redirect.php
+++ adm-checkout-redirect.php
@@ -1,16 +1,205 @@
 <?php
 /*
-Plugin Name: iDesigns Payment Complete Redirect
-Plugin URI: https://ibbotsondesigns.com.au/
+Plugin Name: adm Payment Complete Redirect
 Description: Redirect after successful payment complete. Global redirect or per category. Requires 'Advanced Custom Fields' plugin for product category functionality..
-Author: iDesigns
-Version: 3.2
+Author: danielV
+Version: 3.0
 Author URI: https://ibbotsondesigns.com.au/
 */
 
+
+defined('ABSPATH') || exit;
+
+class admCheckoutRedirectUpdateChecker {
+  
+  public string $plugin_slug;
+  public string $version;
+  public string $cache_key;
+  public bool   $cache_allowed;
+  
+  public function __construct() {
+    
+    $this->plugin_slug   = plugin_basename(__DIR__);
+    $this->version       = '1.1';
+    $this->cache_key     = 'adm_custom_upd';
+    $this->cache_allowed = true;
+    
+    add_filter('plugins_api', array($this, 'info'), 20, 3);
+    add_filter('site_transient_update_plugins', array($this, 'update'));
+    add_action('upgrader_process_complete', array($this, 'purge'), 10, 2);
+    
+  }
+  
+  /**
+   * @throws JsonException
+   */
+  public function request() {
+    
+    $remote = get_transient($this->cache_key);
+    
+    if (false === $remote || !$this->cache_allowed) {
+      
+      $remote = wp_remote_get(
+        'https://demo.alldigitalmarketing.com.au/plugins/adm-checkout-redirect/info.json',
+        array(
+          'timeout' => 10,
+          'headers' => array(
+            'Accept' => 'application/json',
+          ),
+        )
+      );
+      
+      if (
+        is_wp_error($remote)
+        || 200 !== wp_remote_retrieve_response_code($remote)
+        || empty(wp_remote_retrieve_body($remote))
+      ) {
+        return false;
+      }
+      
+      set_transient($this->cache_key, $remote, DAY_IN_SECONDS);
+      
+    }
+    
+    return json_decode(wp_remote_retrieve_body($remote), false, 512, JSON_THROW_ON_ERROR);
+    
+  }
+  
+  
+  /**
+   * @throws JsonException
+   */
+  public function info($res, $action, $args) {
+    
+    // print_r( $action );
+    // print_r( $args );
+    
+    // do nothing if you're not getting plugin information right now
+    if ('plugin_information' !== $action) {
+      return $res;
+    }
+    
+    // do nothing if it is not our plugin
+    if ($this->plugin_slug !== $args->slug) {
+      return $res;
+    }
+    
+    // get updates
+    $remote = $this->request();
+    
+    if (!$remote) {
+      return $res;
+    }
+    
+    $res = new stdClass();
+    
+    $res->name           = $remote->name;
+    $res->slug           = $remote->slug;
+    $res->version        = $remote->version;
+    $res->tested         = $remote->tested;
+    $res->requires       = $remote->requires;
+    $res->author         = $remote->author;
+    $res->author_profile = $remote->author_profile;
+    $res->download_link  = $remote->download_url;
+    $res->trunk          = $remote->download_url;
+    $res->requires_php   = $remote->requires_php;
+    $res->last_updated   = $remote->last_updated;
+    
+    $res->sections = array(
+      'description'  => $remote->sections->description,
+      'installation' => $remote->sections->installation,
+      'changelog'    => $remote->sections->changelog,
+    );
+    
+    if (!empty($remote->banners)) {
+      $res->banners = array(
+        'low'  => $remote->banners->low,
+        'high' => $remote->banners->high,
+      );
+    }
+    
+    return $res;
+    
+  }
+  
+  /**
+   * @throws JsonException
+   */
+  public function update($transient) {
+    
+    if (empty($transient->checked)) {
+      return $transient;
+    }
+    
+    $remote = $this->request();
+    
+    if (
+      $remote
+      //        && version_compare($remote->requires_php, PHP_VERSION, '<')
+      && version_compare($this->version, $remote->version, '<')
+      //        && version_compare($remote->requires, get_bloginfo('version'), '<=')
+    ) {
+      $res              = new stdClass();
+      $res->slug        = $this->plugin_slug;
+      $res->plugin      = plugin_basename(__FILE__); // misha-update-plugin/misha-update-plugin.php
+      $res->new_version = $remote->version;
+      $res->tested      = $remote->tested;
+      $res->package     = $remote->download_url;
+      
+      $transient->response[$res->plugin] = $res;
+      
+    }
+    
+    return $transient;
+    
+  }
+  
+  public function purge($upgrader, $options): void {
+    
+    if (
+      $this->cache_allowed
+      && 'update' === $options['action']
+      && 'plugin' === $options['type']
+    ) {
+      // just clean the cache when new plugin version is installed
+      delete_transient($this->cache_key);
+    }
+    
+  }
+  
+  
+}
+
+new admCheckoutRedirectUpdateChecker();
+
 require('inc/adm-checkout-redirect-woo-settings.php');
 require('inc/adm-checkout-acf.php');
-require('inc/adm-plugin-updater.php');
+
+add_filter('plugin_row_meta', function ($links_array, $plugin_file_name, $plugin_data, $status) {
+  
+  if (strpos($plugin_file_name, basename(__FILE__))) {
+    
+    $links_array[] = sprintf(
+      '<a href="%s" class="thickbox open-plugin-details-modal">%s</a>',
+      add_query_arg(
+        array(
+          'tab'       => 'plugin-information',
+          'plugin'    => plugin_basename(__DIR__),
+          'TB_iframe' => true,
+          'width'     => 772,
+          'height'    => 788,
+        ),
+        admin_url('plugin-install.php')
+      ),
+      __('View details')
+    );
+    
+  }
+  
+  return $links_array;
+  
+}, 25, 4);
+
 
 add_filter('plugin_action_links_adm-checkout-redirect/adm-checkout-redirect.php', 'nc_settings_link');
 function nc_settings_link($links) {
--- inc/adm-plugin-updater.php
+++ inc/adm-plugin-updater.php
@@ -1,8 +1,1 @@
 <?php
-require('updater/boot.php');
-require('updater/updater.php');
-require('updater/theme-updater.php');
-require('updater/plugin-updater.php');
-
-$updater = MakeitWorkPress\WP_Updater\Boot::instance();
-$updater->add(['type' => 'plugin', 'source' => 'https://github.com/All-Digital-Marketing/adm-checkout-redirect']);
